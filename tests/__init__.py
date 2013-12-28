3248f150f4d429153135e595a1faff4c38b09b9e
g561 <grzesmakosa@gmail.com>
2013-12-28 16:37:47 +0100

    fixed running programs with new command line

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,27 +1,33 @@
 
 import unittest
-import wymiot
+from wymiot import Wymiot
 
 
 class DoesDoSth(unittest.TestCase):
 
     def test_nothing(self):
         path_to_nothing = '../examples/nothing/'
-        result = wymiot.Wymiot.run_program(path_to_nothing, 'nothing', '')
-
-        self.assertEquals(result, 'nothing\n')
+        wymiot = Wymiot("nothing", path_to_nothing, path_to_nothing)
+        wymiot.run_program(lambda result: self.assertEquals(result, 'nothing\n'))
 
     def test_read_files(self):
         path_to_test_dir = '../examples/fib/tests/'
-        result = wymiot.Wymiot.list_input_files(path_to_test_dir)
+        path_to_exec_dir = '../examples/fib'
+        fib_exec = "fib"
+        wymiot = Wymiot(fib_exec, path_to_exec_dir, path_to_test_dir)
+        result = wymiot.list_input_files()
 
         self.assertListEqual(result, [('test01.in', 'test01.out')])
 
     def ignore_test_do_sth0(self):
-        path_to_fib = '../examples/fib/'
-        result = wymiot.Wymiot.run_program(path_to_fib, 'fib', 'tests/test01.in')
+        path_to_exec_dir = '../examples/fib/'
+        fib_exec = 'fib'
+        test_instance = 'tests/test01.in'
+        wymiot = Wymiot(fib_exec, path_to_exec_dir, test_instance)
+
         expected = '1 13 233'
-        self.assertEquals(result, expected)
+        wymiot.run_program(lambda result: self.assertEquals(result, expected))
+
 
 def main():
     unittest.main()
--- wymiot/__init__.py
+++ wymiot/__init__.py
@@ -18,30 +18,38 @@ from docopt import docopt
 
 class Wymiot(object):
 
-    @staticmethod
-    def list_input_files(tests_dir):
-        ins = [f for f in os.listdir(tests_dir) if re.search('[.]*.in', f) ]
+    def __init__(self, exec_path, exec_name, test_path):
+        self.exec_name = exec_name
+        self.exec_path = exec_path
+        self.test_path = test_path
+
+    def list_input_files(self):
+        ins = [f for f in os.listdir(self.test_path) if re.search('[.]*.in', f) ]
         ous = [(fin, fin.replace('.in', '.out')) for fin in ins]
         return ous
 
-    @staticmethod
-    def run_program(path, name, tests_dir):
-        output = subprocess.check_output(path + name, stderr=subprocess.STDOUT)
-        return output.decode()
+    def run_program(self, end_listener):
+        output = subprocess.check_output(to_path(self.exec_path, self.exec_name), stderr=subprocess.STDOUT)
+        decoded_output = output.decode()
+        end_listener(decoded_output)
+
+
+def to_path(dir_path_0, dir_path_1):
+    return "%s/%s" % (dir_path_0, dir_path_1)
 
 
 def main():
     arguments = docopt(__doc__, version="0.0.1")
-    prog_path = arguments.get('--program', ".")
-    prog_name = arguments.get('--exec-name', os.path.basename(os.getcwd()))
-    test_dir = arguments.get('--test_dir', 'tests')
+    prog_path = "." if arguments['--exec-path'] is None else arguments['--exec-path']
+    prog_name = os.path.basename(os.getcwd()) if arguments['--exec-name'] is None else arguments.get('--exec-name')
+    test_dir = 'tests' if arguments['--test_dir'] is None else arguments.get('--test_dir')
 
-    if not os.path.isfile(prog_name):
+    if not os.path.isfile(to_path(prog_path, prog_name)):
         print("The program %s does not exists" % (prog_path + "/" + prog_name))
         return
 
-    result = Wymiot.run_program(prog_path, prog_name, test_dir)
-    print(result)
+    wymiot = Wymiot(prog_path, prog_name, test_dir)
+    wymiot.run_program(print)
 
 if __name__ == '__main__':
     main()
